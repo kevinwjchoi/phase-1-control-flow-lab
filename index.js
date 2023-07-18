7ad607df21c0674454f27dc15e14020d3fec9bfb
Kevin Choi <kevinnwjchoi@gmail.com>
2023-07-18 17:55:17 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someFeet){
+  if (someFeet <= 400){
+    return 'This one is on me!';
+  }
+  else if (someFeet <= 2000){
+    return 'That will be twenty bucks.';
+  }
+  else if (someFeet <=2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else (someFeet >2500) 
+    return 'No can do.';
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(somePlace){
+  const city = somePlace === 'NYC' ? 'Ok, sounds good.' : 'No go.'; 
+  return city; 
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+
 }
